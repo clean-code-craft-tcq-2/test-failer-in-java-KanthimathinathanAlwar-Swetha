377656d2cdfcab29552e1d342aa7045fc59de2e0
KanthimathinathanAlwar-Swetha <k.a.swetha850@gmail.com>
2022-02-01 20:48:32 +0530

    Pass the failing test Assignment

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -2,23 +2,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class misaligned {
-    static Map<String, String> printColorMap() {
-	Map<String, String> colorPair = new HashMap<String, String>();
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-		colorPair.put(String.valueOf(i) + String.valueOf(j), majorColors[i] + " " + minorColors[i]);
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
-            }
-        }
-        return colorPair;
+  static Map<Integer, String> colorPair = new HashMap<>();
+  static String[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+  static String[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+  static void printColorMap(final int pairNumber, final int i, final int j) { 
+    System.out.printf("%3d | %8s | %8s%n", pairNumber, majorColors[i - 1], minorColors[j - 1]);
+  }
+
+  static Map<Integer, String> getColorPair() {
+    int i = 1, j = 1;
+    for (i = 1; i <= 5; i++) {
+      for (j = 1; j <= 5; j++) {
+        int pairNumber = ((i - 1) * 5) + j;
+        colorPair.put(pairNumber, majorColors[i - 1] + " " + minorColors[j - 1]);
+        printColorMap(pairNumber, i, j);
+      }
     }
-    public static void main(String[] args) { 
-	Map<String, String> result = printColorMap();
-        assert(result.get("23").equals("Black Brown"));
-	assert(result.get("02").equals("White Orange"));
-        System.out.println("All is well (maybe!)");
-    }
+    return colorPair;
+  }
+  public static void main(String[] args) { 
+  Map<Integer, String> colorMap = getColorPair();
+      assert(colorMap.get(14).equals("Black Brown"));
+      assert(colorMap.get(2).equals("White Orange"));
+      System.out.println("All is well (maybe!)");
+  }
 }
--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,6 +1,6 @@
 public class tshirts {
     static String size(int cms) {
-        if(cms < 38) {
+        if(cms <= 38) {
             return "S";
         } else if(cms > 38 && cms < 42) {
             return "M";
@@ -12,7 +12,9 @@ public class tshirts {
     public static void main(String[] args) { 
         assert(size(37).equals("S"));
         assert(size(38).equals("S"));
-        assert(size(40).equals("M"));
+	assert(size(39).equals("M"));
+        assert(size(41).equals("M"));
+	assert(size(42).equals("L"));
         assert(size(43).equals("L"));
         System.out.println("All is well (maybe!)");
     }
